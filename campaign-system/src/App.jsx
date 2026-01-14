98e4f55497ecf6ed7f065d85f4c1ae5dd357250e
Abijit Agamanandan <abijit.agaman@gmail.com>
2026-01-14 18:04:34 +0000

    Add basename to Router for GitHub Pages support

--- campaign-system/src/App.jsx
+++ campaign-system/src/App.jsx
@@ -12,7 +12,7 @@ import Support from './pages/Support';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
